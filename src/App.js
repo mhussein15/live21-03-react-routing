cf7ff40ecfd6a495864d767555ee815ad176b085
Mohammad Hussein <mhalfakih15@gmail.com>
2021-02-01 10:14:30 +0300

    Task Done

--- src/App.js
+++ src/App.js
@@ -6,47 +6,6 @@ import { useState } from "react";
 import { Route, Switch ,Redirect} from "react-router-dom";
 
 const App = ({ instructors }) => {
-  // const homemadeRouter = () => {
-  //   switch (currentPage) {
-  //     case "/":
-  //       return <Home instructors={instructors} goTo={setCurrentPage} />;
-
-  //     case "/instructors/hamza":
-  //       return (
-  //         <InstructorProfile
-  //           instructors={instructors}
-  //           instructorSlug="hamza"
-  //           goTo={setCurrentPage}
-  //         />
-  //       );
-
-  //     case "/instructors/laila":
-  //       return (
-  //         <InstructorProfile
-  //           instructors={instructors}
-  //           instructorSlug="laila"
-  //           goTo={setCurrentPage}
-  //         />
-  //       );
-
-  //     case "/instructors/hasan":
-  //       return (
-  //         <InstructorProfile
-  //           instructors={instructors}
-  //           instructorSlug="hasan"
-  //           goTo={setCurrentPage}
-  //         />
-  //       );
-
-  //     case "/404":
-  //       return <NotFound goTo={setCurrentPage} />;
-
-  //     default:
-  //       setCurrentPage("/404");
-  //       break;
-  //   }
-  // };
-
   return (
     <AppWrapper>
       <Switch>
